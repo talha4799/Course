85ac861b59e281c2e6852eb2fddbc7a3cbf0157e
talha4799 <talhamriza47@gmail.com>
2025-12-30 15:10:17 +0500

    array slice and splice method

---
+++ JavaScript/fifteen.js
@@ -0,0 +1,15 @@
+
+// Array Search Methods
+
+// Slice
+
+// let fruits = ["apple", "banana", "orange", "grape", "mango"];
+// let khata = fruits.slice(2);
+// console.log(khata); // Output: ["orange", "grape", "mango"]
+// console.log(khata.slice(1)); // Output: ["apple", "banana", "orange", "grape", "mango"]
+// console.log(fruits)
+
+// Splice
+// fruits = ["apple", "banana", "orange", "grape", "mango"];
+// let khata = fruits.toSpliced(2, 1, "kiwi", "pear");
+// console.log(khata);
