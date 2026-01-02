9f967b8c6383d2e5518a1c09dd6b1731d211ee69
talha4799 <talhamriza47@gmail.com>
2026-01-02 14:58:07 +0500

    interation add

--- JavaScript/fifteen.js
+++ JavaScript/fifteen.js
@@ -1,13 +1,59 @@
+// Array Spread Method Iteration
+let quarter1 = ["January", "February", "March"];
+let quarter2 = ["April", "May", "June"];
+let quarter3 = ["July", "August", "September"];
+let quarter4 = ["October", "November", "December"];
+let full_year = [...quarter1, ...quarter2, ...quarter3, ...quarter4];
+console.log(full_year);
+// Array with Methods
+// let fruits = ["apple", "banana", "orange", "grape", "mango"]
+// let fruits_with = fruits.with(2, "kiwi");
+// console.log(fruits_with);
+// console.log(fruits);
+// Array Entries Method
+// let fruits = ["apple", "banana", "orange", "grape", "mango"];
+// let fruits_entries = fruits.entries();
+
+// for (x of fruits_entries) {
+//     txt = "The fruit index and name are: ";
+//     print_entry = txt + x;
+//     console.log(print_entry);
+// }
+
+// console.log(fruits);
+// Arrays Keys Method
+// let fruits = ["apple", "banana", "orange", "grape", "mango"];
+// let fruits_keys = fruits.keys();
+// for (let x of fruits_keys) {
+//     txt = "The fruits indexes are:";
+//     print_index =txt + x;
+//     console.log(print_index);
+// }
+
+// Array Filter Method
+// let numbers = [67, 50, 80, 76, 10, 25, 30, 45, 60, 75];
+// let filter_num = numbers.filter(filter_func)
+
+// function filter_func(value, array) {
+//     return value > 18;
+// }
+
+// console.log(filter_num.sort());
+
+// Array FlatMap Method
+// let numbers = [1, 2, 3, 4, 5];
+// let num_result = numbers.flatMap((x) => [x * 2]);
+// console.log(num_result);
 // Array ForEach Method
 
-let numbers = [10, 20, 30, 40, 50];
-let map = numbers.map(mapFunc);
+// let numbers = [10, 20, 30, 40, 50];
+// let map = numbers.map(mapFunc);
 
-function mapFunc(value) {
-    return value * 2;
-}
-console.log(map);
-document.getElementById("map").innerHTML = map;
+// function mapFunc(value) {
+//     return value * 2;
+// }
+// console.log(map);
+// document.getElementById("map").innerHTML = map;
 // let num_find = numbers.forEach(forEachFunc);
 // function forEachFunc(value, index, array) {
 //     if (value > 35) {
